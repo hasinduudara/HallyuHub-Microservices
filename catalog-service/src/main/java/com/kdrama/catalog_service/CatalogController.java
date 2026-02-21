b4b69c22245ee3036cecdb9e17035673d811e11a
Hasindu Udara <151644598+hasinduudara@users.noreply.github.com>
2026-02-21 09:24:44 +0530

    update drama list

--- catalog-service/src/main/java/com/kdrama/catalog_service/CatalogController.java
+++ catalog-service/src/main/java/com/kdrama/catalog_service/CatalogController.java
@@ -14,11 +14,16 @@ import java.util.List;
 public class CatalogController {
 
     private List<CatalogItem> catalogItems = Arrays.asList(
-            new CatalogItem("1", "City Hunter", "Action/Romance", 2011, 4.8),
-            new CatalogItem("2", "Stranger Things", "Sci-Fi/Mystery", 2016, 4.7),
-            new CatalogItem("3", "The Witcher", "Action/Fantasy", 2019, 4.5),
-            new CatalogItem("4", "The Lord of the Rings", "Historical/Action", 2001, 4.9),
-            new CatalogItem("5", "Vagabond", "Action/Thriller", 2019, 4.6)
+            new CatalogItem("1", "Vincenzo", "Action/Crime", 2021, 4.8),
+            new CatalogItem("2", "Healer", "Action/Romance/Mystery", 2014, 4.7),
+            new CatalogItem("3", "The K2", "Action/Thriller", 2016, 4.6),
+            new CatalogItem("4", "Goblin", "Fantasy/Romance", 2016, 4.8),
+            new CatalogItem("5", "Itaewon Class", "Drama/Revenge", 2020, 4.6),
+            new CatalogItem("6", "Kingdom", "Historical/Action/Horror", 2019, 4.7),
+            new CatalogItem("7", "Taxi Driver", "Action/Crime", 2021, 4.7),
+            new CatalogItem("8", "Signal", "Thriller/Mystery", 2016, 4.8),
+            new CatalogItem("9", "Arthdal Chronicles", "Fantasy/Action", 2019, 4.5),
+            new CatalogItem("10", "Descendants of the Sun", "Action/Romance", 2016, 4.6)
     );
 
     @GetMapping
